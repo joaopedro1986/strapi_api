7c95cd752b6e8cbf752161bea9b7bdd0d473daf4
Joao Pedro <joao.ricardo88@gmail.com>
2021-02-28 23:13:50 +0000

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bca7d237afaca2f1655716ac9dcf0df5'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4d784659-a7ea-41a2-bb9e-8068add7cf09'
+};
